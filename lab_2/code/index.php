2859839f08b25b7ee38b9a5150c61ebbb16d5431
ppppyzhkoff <pavel071204@gmail.com>
2024-03-19 17:17:40 +0200

    Using the concatenation operator to added a string to the $order variable

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -1,2 +1,15 @@
 <?php
-echo phpinfo();
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+
+// Write your code here:
+//1
+$order = &$very_bad_unclear_name;
+$order .= ", and 10 chicken legs";
+
+
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
