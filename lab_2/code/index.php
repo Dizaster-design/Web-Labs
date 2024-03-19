e113c5cf1acbdff08671f028a735090d4d2c88f7
ppppyzhkoff <pavel071204@gmail.com>
2024-03-19 17:48:26 +0200

    Showed integer and floating point addition and subtraction operations

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -1,10 +1,11 @@
 <?php
+//task 1
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
 
 // Write your code here:
-//1
+
 $order = &$very_bad_unclear_name;
 $order .= ", and 10 chicken legs";
 
@@ -12,4 +13,26 @@ $order .= ", and 10 chicken legs";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+
+//task 2
+$variable = 1;
+echo $variable;
+echo "\n";
+
+$number = 12.1;
+echo $number;
+echo "\n";
+
+echo 5 + 9 - 2;
+echo "\n";
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "Разница между расходами в этом месяце и прошлом ", $lastMonth - $thisMonth;
+echo "\n\n";
+
+
+
+
 
