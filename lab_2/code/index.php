49347eef39b75778ce189ef55e8fa7e284e81cb1
Dizaster-design <60186936+Dizaster-design@users.noreply.github.com>
2024-03-19 23:34:32 +0200

    The use of various combinations of functions is shown.

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -232,6 +232,31 @@ for($i = 1; $i <= $pyramidSize; $i++) {
 }
 echo "\n\n";
 
+//task 20
+$numbers = [3, 1, 4, 5, 6, 7, 7, 8, 8];
+$average = array_sum($numbers) / sizeof($numbers);
+echo $average, "\n";
+
+$end = 100;
+$sum = ($end * ($end + 1)) / 2;
+echo $sum;
+
+$sqrtNumbers = array_map("sqrt", $numbers);
+print_r($sqrtNumbers);
+echo "\n";
+
+$numbers = range(1, 26);
+$letters = range('a', 'z');
+$result = array_combine($letters, $numbers);
+print_r($result);
+echo "\n";
+
+$string = '1234567890';
+$pairs = str_split($string, 2);
+$arr = array_map("intval",$pairs);
+$sum = array_sum($arr);
+echo $sum;
+
 
 
 
