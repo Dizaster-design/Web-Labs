c0526e249ca456085e190406718fbdaca4adcf47
ppppyzhkoff <pavel071204@gmail.com>
2024-03-30 18:35:16 +0200

    Write the regular function for replacing symbols

--- lab_3/index.php
+++ lab_3/index.php
@@ -6,3 +6,11 @@ $matches = [];
 $count = preg_match_all($regexp, $str, $matches);
 echo var_dump($matches), "\n";
 
+$str = 'a1b2c3';
+$regexp = '/[0-9]+/';
+$result = preg_replace_callback($regexp, function($match) {return $match[0]**3;}, $str);
+echo $result, "\n";
+
+
+
+
