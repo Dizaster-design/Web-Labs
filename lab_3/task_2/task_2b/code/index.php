1e79988a39c3f512adc90920d2ce53bf283e8574
ppppyzhkoff <pavel071204@gmail.com>
2024-03-30 22:43:59 +0200

    task_2 step2

---
+++ lab_3/task_2/task_2b/code/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="step2.php" method="post">
+            <label for="LastName"> Your LastName: </label>
+            <input type="text" name="LastName"> <br>
+            <label for="Name"> Your Name: </label>
+            <input type="text" name="Name"> <br>
+            <label for="Age"> Your age: </label>
+            <input type="text" name="Age"> <br>
+            <input type="submit" value="Start step 2"> <br>
+        </form>
+    </div>
+</body>
+</html>
